eb64e03176878a049b668073edc37c42eabaf08e
MHR-2477380 <ru.ru.ru.247738@gmail.com>
2020-01-23 17:49:41 +0900

    [Finish]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+
+  def index
+  	@book = Book.new
+    # 記事を全件取得
+    @books = Book.all.order(id: "asc")
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  	# ストロングパラメータ
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice:"Book was successfully created."
+    else
+      @books = Book.all.order(id: "asc")
+      render :index
+    end
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+       redirect_to book_path(book), notice:"Book was successfully updated."
+    else render :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+  def book_params
+	params.require(:book).permit(:title, :body)
+  end
+
+end
